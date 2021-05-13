38da2dc7ea8dd090597d427c57b9cd694726dc39
Adolfo <allorenter@gmail.com>
2021-05-13 20:10:36 +0200

    Loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import { Spin } from "antd";
+import { LoadingOutlined } from '@ant-design/icons';
+
+function Loading(props) {
+    const antIcon = <LoadingOutlined type="loading" style={{ fontSize: props.size, color: props.color }} spin />;
+    return (
+        <Spin style={{ textAlign: "center" }} spinning={props.active} indicator={antIcon}>
+            {props.children}    
+        </Spin>
+    );
+}
+
+export default Loading;
