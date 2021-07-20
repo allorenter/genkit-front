df4f3120c090ac6fa94ed25bec80efdfec5d4fc3
Adolfo <allorenter@gmail.com>
2021-07-20 17:28:04 +0200

    Compila las variables less de antd en los scripts start y build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import 'antd/dist/antd.css';
 import Loading from './components/Loading';
 import GeneratorData from './components/GeneratorData';
 import Header from './components/Header';
